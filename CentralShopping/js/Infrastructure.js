f76f090fff5bd3cd8684bc63f562c5620525b6da
Mostafa <Mostafa_stn@yahoo.com>
2018-12-25 00:26:20 +0330

    add diynamic menu from json data by jquery

---
+++ CentralShopping/js/Infrastructure.js
@@ -0,0 +1,49 @@
+﻿function readTextFileJS(file, callback) {
+
+
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+};
+
+function readTextFile(file, callback) {
+    
+    $.getJSON(file, function (data) {
+        var items = [];
+        $.each(data, function (key, val) {
+            items.push("<li id='" + val.zipcode + "'>" + val.name + "(" + val.zipcode + ")</li>");
+        });
+        callback(items)
+    });
+};
+
+
+
+//usage:
+function loadMenuJS(callback) {
+
+    readTextFileJS("../Menu/Menu.json", function (text) {
+
+        var data = JSON.parse(text);
+        callback(data);
+    });
+
+
+};
+
+function loadMenu(callback) {
+    
+    var file = '../Menu/Menu.json';
+    readTextFile(file, function (text) {
+        callback(text);
+    });
+
+
+};
